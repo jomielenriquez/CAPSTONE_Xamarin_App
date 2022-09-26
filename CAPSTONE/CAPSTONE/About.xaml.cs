2e4e131179ba119b6b6ec937992f176dbd393174
JomielE <JomielE@MBTMNL-JOMIEE>
2022-09-26 20:42:32 +0800

    9/26 Update

--- CAPSTONE/CAPSTONE/About.xaml.cs
+++ CAPSTONE/CAPSTONE/About.xaml.cs
@@ -25,6 +25,7 @@ namespace CAPSTONE
 
             viewModel.IsLoading = false;
             LoadingModalVisibility(false);
+            lable_title.Text = "Tanauan City is home to scenic nature's beauty, attractive investment opportunities, rich culture and historical heritage, superb adventure and vigoruos industrialization. The Natural wealth of its land and the dynamism and resourcefulness of its people pave the way to competitiveness, bringing the city to the crest of progress and advancement, In the midst of developement, we envision to share the exquisite and enticing places being painstakingly preserved and nurtured i the city. We seek to share its rich history, culture heritage, natural attractions and th values, traits and traditions of a warm and friendly citizenry. Inspired by the industrious of Tanauenos, we preserve in moving the city towards attainment of its vision in sustainable agricultural, industrial, commercial and tourism development, people empowerment and well-being and good governance. We invite you to be part of a new experience, as we pursue tovether the continuous groeth and growth and progress of the city.";
         }
         public void OnButtonCancel(object sender, EventArgs args)
         {
---
+++ CAPSTONE/CAPSTONE/Services/IPrintService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPSTONE.Services
+{
+    public interface IPrintService
+    {
+        IList<string> GetDeviceList();
+        Task Print(string deviceName, string text);
+    }
+}
