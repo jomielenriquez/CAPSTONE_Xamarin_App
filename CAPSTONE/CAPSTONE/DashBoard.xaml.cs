b7d3d0446a836966a8a824ba942688b16feb1c3a
JomielE <JomielE@MBTMNL-JOMIEE>
2022-09-26 08:25:42 +0800

    Add project files.

---
+++ CAPSTONE/CAPSTONE/DashBoard.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CAPSTONE
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DashBoard : ContentPage
+    {
+        string UID = string.Empty;
+        public DashBoard(string uid)
+        {
+            InitializeComponent();
+            UID = uid;
+            lable_title.Text += UID;
+        }
+    }
+}
---
+++ CAPSTONE/CAPSTONE/Model/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAPSTONE.Model
+{
+    //internal class LoginViewModel
+    //{
+    //}
+    public class LoginViewModel
+    {
+        public bool IsLoading { get; set; }
+    }
+    public class UID
+    {
+        public string uid { get; set; }
+    }
+}
